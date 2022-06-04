7898b4cfec62f8eb15a0fa63eb7ec04f402f87d0
Scott Hallock <scott@hallocks.net>
2022-06-04 16:16:06 +0000

    Fix required_providers block

--- terraform/gcp/game-server.tf
+++ terraform/gcp/game-server.tf
@@ -1,24 +1,26 @@
 terraform {
   required_providers {
-    source = "hashicorp/google"
-    version = "4.23.0"
+  google = {
+      source  = "hashicorp/google"
+      version = "4.23.0"
+    }
   }
 }
 
 provider "google" {
   credentials = file("credential.json")
-  project = "game-server-352313"
-  region = "us-central1"
-  zone = "us-central1-c"
+  project     = "game-server-352313"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
 resource "google_compute_network" "game_server_network" {
-  name = "game-server-network"
+  name                    = "game-server-network"
   auto_create_subnetworks = "true"
 }
 
 resource "google_compute_firewall" "game_server_firewall" {
-  name = "game-server-firewall"
+  name    = "game-server-firewall"
   network = google_compute_network.game_server_network.self_link
 
   allow {
@@ -27,17 +29,17 @@ resource "google_compute_firewall" "game_server_firewall" {
 
   allow {
     protocol = "tcp"
-    ports = ["3389"]
+    ports    = ["3389"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_instance" "game_server" {
-  name = "game-server"
-  machine_type = "e2-standard-2"
+  name                      = "game-server"
+  machine_type              = "e2-standard-2"
   allow_stopping_for_update = true
-  labels = {purpose = "game-server-prod"}
+  labels                    = { purpose = "game-server-prod" }
 
   scheduling {
     automatic_restart = false
@@ -45,7 +47,7 @@ resource "google_compute_instance" "game_server" {
 
   boot_disk {
     initialize_params {
-      size = 30
+      size  = 30
       image = "windows-cloud/windows-2022"
     }
   }
